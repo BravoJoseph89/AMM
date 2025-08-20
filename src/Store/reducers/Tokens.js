c32f73a4e73b49dfaa8553d2f9b6824800818a28
BravoJoseph89 <bravojoseph89@gmail.com>
2025-08-19 19:03:41 -0700

    Loads data into redux

---
+++ src/Store/reducers/Tokens.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const tokens = createSlice({
+    name: `tokens`,
+    initialState: {
+        connection: [],
+        symbols: [],
+        balances: [0, 0]
+    },
+    reducers: {
+        setContracts: (state, action) => {
+            state.connection = action.payload
+        },
+        setSymbols: (state, action) => {
+            state.symbols = action.payload
+        },
+        balancesLoaded: (state, action) => {
+            state.balances = action.payload
+        }
+    }
+})
+
+export const { setContracts, setSymbols, balancesLoaded } = tokens.actions;
+
+export default tokens.reducer;
