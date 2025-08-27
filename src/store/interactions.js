2de550998a21c21b2b043ea977ccea8782379d2a
BravoJoseph89 <bravojoseph89@gmail.com>
2025-08-27 10:38:07 -0700

    Removes liquidity form

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -18,6 +18,9 @@ import {
     depositRequest,
     depositSuccess,
     depositFail,
+    withdrawRequest,
+    withdrawSuccess,
+    withdrawFail,
     swapRequest,
     swapSuccess,
     swapFail,
@@ -106,6 +109,24 @@ export const loadTokens = async (provider, chainId, dispatch) => {
         dispatch(depositSuccess(transaction.hash))
     } catch (error) {
         dispatch(depositFail())
+    }
+  }
+
+
+  // ---------------------------------------------------------------
+  // REMOVE LIQUIDITY
+  export const removeLiquidity = async (provider, amm, shares, dispatch) => {
+    try{
+        dispatch(withdrawRequest())
+        
+        const signer = await provider.getSigner()
+    
+        let transaction = await amm.connect(signer).removeLiquidity(shares)
+        await transaction.wait()
+        
+        dispatch(withdrawSuccess(transaction.hash))
+    } catch (error) {
+        dispatch(withdrawFail())
     }
   }
 
